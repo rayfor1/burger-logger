24753f64084877be3570aa5685478697b6a887db
github username <github email address>
2020-05-15 22:52:00 -0400

    created burger.js within a dir called models

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+const orm = require("../config/orm");
+
+const burger = {
+    create: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            cb(res);
+        })
+    },
+    update: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        })
+    },
+    select: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
