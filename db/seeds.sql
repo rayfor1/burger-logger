2236161d36328ccc6a0c891127504478fb516d13
Raymond Anuforo Jr <anufororaymond@yahoo.com>
2020-10-02 12:54:29 -0400

    updated seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,8 +3,8 @@ USE burgers_db;
 -- List of Burgers: 
 INSERT INTO burgers(burger_name)
 VALUES
-    ("Classic Burger"),
-    ("Bacon Burger"),
-    ("Veggie Burger"),
-    ("Chicken Burger"),
-    ("Ray's Special")
+    ("Classic Burger", false),
+    ("Bacon Burger", false),
+    ("Veggie Burger", false),
+    ("Chicken Burger", false),
+    ("Ray's Special", false)
