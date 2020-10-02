9cce339b5153ebc4e3b777d62f9d0fceeb907141
Raymond Anuforo Jr <anufororaymond@yahoo.com>
2020-10-02 14:37:56 -0400

    created the orm

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,6 @@
+// Here is the O.R.M. where you write functions that takes inputs and conditions
+// and turns them into database commands like SQL.
+
 const connection = require("../config/connection.js");
 
 function printQuestionMarks(num) {
@@ -29,23 +32,50 @@ var orm = {
     selectAll: (table, cb) => {
         var queryString = `SELECT * FROM ${table};`;
         connection.query(queryString, (err, result) => {
-            if (err) throw err
+            if (err) {
+                throw err;
+            }
 
             cb(result);
         })
     },
+    // values is an array of values that we want to save to columns
+    // columns are the columns we want to insert the values into
     insertOne: (table, columns, values, cb) => {
         var queryString = `INSERT INTO ${table}(${columns.toString()}) VALUES (${printQuestionMarks(values.length)});`;
+
+        console.log(queryString)
         connection.query(queryString, values, (err, result) => {
-            if (err) throw err;
+            if (err) {
+                throw err;
+            }
 
             cb(result);
         })
     },
+    // objColVals would be the columns and values that we want to update
+    // an example of objColVals would be {name: panther, sleepy: true}
     updateOne: (table, objColVals, condition, cb) => {
         var queryString = `UPDATE ${table} SET ${objectToSql(objColVals)} WHERE ${condition};`;
+
+        console.log(queryString)
         connection.query(queryString, (err, result) => {
-            if (err) throw err;
+            if (err) {
+                throw err;
+            }
+            
+            cb(result);
+        })
+    },
+
+    //create a delete function to help manage the conditions:
+    deleteOne: (table, condition, cb) => {
+        var queryString = `DELETE FROM ${table} WHERE ${condition};`;
+        console.log(queryString)
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
             
             cb(result);
         })
