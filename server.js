5eb33b34e67d5dd47b3199dd11964595ee4c7e2e
Raymond Anuforo Jr <anufororaymond@yahoo.com>
2020-10-02 13:13:56 -0400

    completed server.js setup

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ var exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 8000;
 var app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body:
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Handlebars setup: 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Routes setup:
+var routes = require("./controllers/burgersController.js");
+app.use(routes);
+
 app.listen(PORT, function() {
     console.log("Listening on http://localhost:" + PORT);
   });
